5709ddce2c927bf39deae1c0b64449fa26992bea
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-10 15:44:45 -0700

    testing netlify deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
-      <p>testing hello</p>
+    <div className="bg-slate-800">
+      <p className='text-3xl text-blue-500'>testing hello</p>
     </div>
   )
 }
