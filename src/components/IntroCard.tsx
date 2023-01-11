f9e56b9e0b4a7ae0adae0432f217a6e24ece2c7c
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-11 13:46:38 -0700

    style: spacing

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -5,11 +5,11 @@ import Nameplate from "./widgets/Nameplate";
 
 const IntroCard = () => {
   return (
-    <div className="flex flex-col md:flex-row-reverse md:w-4/5 md:h-[90%] h-screen bg-white md:drop-shadow-custom-large md:rounded-md">
-      <div className="h-2/5 md:h-full md:w-1/2 md:rounded-tr-md md:rounded-br-md bg-[#0F1922] text-white">
+    <div className="flex h-full w-full flex-col md:h-[90%] md:w-4/5 md:flex-row-reverse md:drop-shadow-custom-large">
+      <div className="h-2/5 bg-[#0F1922] text-white md:h-full md:w-1/2 md:rounded-r-md">
         This should be an image
       </div>
-      <div className="flex flex-col items-start">
+      <div className="flex flex-col items-start bg-white px-8 md:w-1/2 md:justify-center md:rounded-l-md md:px-20">
         <Nameplate />
         <IntroCardTypewriterHeader />
         <AboutBlurb />
