81c3a54520080c12c33b011b290f71ed23ea25db
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-11 13:46:10 -0700

    style: spacing

--- src/components/widgets/AboutBlurb.tsx
+++ src/components/widgets/AboutBlurb.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const AboutBlurb = () => {
   return (
-    <p className="font-robotoCondensed text-xl text-left font-light mx-8">
-      Full Stack Web Developer based in Canada. 
+    <p className="mb-8 text-left font-robotoCondensed text-xl font-light">
+      Full Stack Web Developer based in Canada.
       <br />
-      Passionate in building applications that suit your business needs. 
+      Passionate in building applications that suit your business needs.
       <br />
-      <br />
-      I love building things on the internet, seeing the world, and going outside!
+      <br />I love building things on the internet, seeing the world, and going
+      outside!
     </p>
   );
 };
