b908a31d9d126a13d5f219b35cfb604eda907688
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-11 17:10:24 -0700

    docs: refactor the introcard

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -1,35 +1,23 @@
 import React from "react";
+import SmallNavbar from "./SmallNavbar";
 import AboutBlurb from "./widgets/AboutBlurb";
 import IntroCardTypewriterHeader from "./widgets/IntroCardTypewriterHeader";
 import Nameplate from "./widgets/Nameplate";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faFile } from "@fortawesome/free-solid-svg-icons";
-import {
-  faGithub,
-  faLinkedin,
-  faMedium,
-} from "@fortawesome/free-brands-svg-icons";
 
 const IntroCard = () => {
   return (
-    <div className="flex h-full w-full flex-col md:h-[90%] md:w-4/5 md:flex-row-reverse md:drop-shadow-custom-large">
-      <div className="h-2/5 bg-[#0F1922] text-white md:h-full md:w-1/2 md:rounded-r-md">
+    <div className="flex h-full w-full flex-col lg:h-[90%] lg:w-4/5 lg:flex-row-reverse lg:justify-between lg:drop-shadow-custom-large">
+      <div className="h-2/5 bg-[#0F1922] text-white lg:h-full lg:w-1/2 lg:rounded-r-md">
         This should be an image
       </div>
-      <div className="flex flex-col items-start bg-white px-8 md:w-1/2 md:justify-center md:rounded-l-md md:px-20">
+      <div className="flex flex-col items-start bg-white px-8 lg:h-full lg:w-1/2 lg:justify-start lg:rounded-l-md lg:px-16">
+        <SmallNavbar />
         <Nameplate />
+        <h1 className="my-2 font-robotoCondensed text-xl font-bold text-azure lg:mb-4 lg:text-2xl 2xl:mb-6">
+          ABOUT
+        </h1>
         <IntroCardTypewriterHeader />
         <AboutBlurb />
-        {/* Below is examples of using the fontawesome icons in span wrappers */}
-        <span className="flex aspect-square h-10 items-center justify-center overflow-hidden rounded-full bg-white drop-shadow-custom-small">
-          <FontAwesomeIcon icon={faFile} size={"xl"} fixedWidth />
-        </span>
-        <span className="flex aspect-square h-10 items-center justify-center overflow-hidden rounded-full bg-white drop-shadow-custom-small">
-          <FontAwesomeIcon icon={faEnvelope} size={"xl"} fixedWidth />
-        </span>
-        <FontAwesomeIcon icon={faGithub} size={"xl"} fixedWidth />
-        <FontAwesomeIcon icon={faLinkedin} size={"xl"} fixedWidth />
-        <FontAwesomeIcon icon={faMedium} size={"xl"} fixedWidth />
       </div>
     </div>
   );
