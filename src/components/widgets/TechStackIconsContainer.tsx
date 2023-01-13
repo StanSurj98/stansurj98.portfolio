38ce9678d24ba8188f608cd5664305c4795b31bf
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-12 21:40:03 -0700

    prettier

--- src/components/widgets/TechStackIconsContainer.tsx
+++ src/components/widgets/TechStackIconsContainer.tsx
@@ -5,22 +5,34 @@ import {
   faHtml5,
   faCss3Alt,
   faNode,
+  faPython,
 } from "@fortawesome/free-brands-svg-icons";
 
 const TechStackIconsContainer = () => {
-  const techIconList = [faReact, faJsSquare, faHtml5, faCss3Alt, faNode];
+  const techIconList = [
+    faReact,
+    faJsSquare,
+    faHtml5,
+    faCss3Alt,
+    faNode,
+    faPython,
+  ];
   const techIconItems = techIconList.map((icon, index) => {
     return (
       <FontAwesomeIcon
         key={index}
-        className="mx-2 text-4xl text-white cursor-pointer duration-200 ease-out hover:scale-110 hover:ease-out hover:text-azure"
+        className="m-2 cursor-pointer text-4xl text-white duration-200 ease-out hover:scale-110 hover:text-azure hover:ease-out"
         icon={icon}
         fixedWidth
       />
     );
   });
 
-  return <div className="flex justify-center items-center mt-4">{techIconItems}</div>;
+  return (
+    <div className="max-w-4/5 mt-4 flex flex-wrap items-center justify-center">
+      {techIconItems}
+    </div>
+  );
 };
 
 export default TechStackIconsContainer;
