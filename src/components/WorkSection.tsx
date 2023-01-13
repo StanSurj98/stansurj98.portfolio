452c705916334f60a97ad00f94cabc27a227cb9c
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-13 16:03:07 -0700

    feat: create work section of site

---
+++ src/components/WorkSection.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WorkSection = () => {
+  return (
+    <section className="relative w-full bg-white py-28 mt-20 lg:mt-0 xl:min-h-screen">
+      <div className="mx-auto w-10/12 text-center 2xl:w-[80%]">
+        <h2 className="fontSize-smallHeader mb-4 font-robotoCondensed uppercase text-azure ">
+          Work
+        </h2>
+        <h3 className="fontSize-smallHeader mb-8 font-robotoCondensed text-darksteel">
+          Hang Around a While!
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default WorkSection;
