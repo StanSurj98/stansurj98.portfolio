d5c058bfff44f5f2fb5c491ee0a64cebf93aeb4f
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-13 16:07:05 -0700

    test: layout for work section of site

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import LandingPage from "./components/LandingPage"
+import LandingPage from "./components/LandingPage";
+import ProjectItems from "./components/ProjectItems";
+import WorkSection from "./components/WorkSection";
 // import Navbar from "./components/Navbar"
 
 function App() {
-
   return (
     <div className="overflow-x-hidden">
       {/* <Navbar></Navbar> */}
       <LandingPage />
-      <div className="h-screen bg-pink-500"></div>
+      <WorkSection />
+      <div className="h-screen w-screen bg-pink-200"></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
