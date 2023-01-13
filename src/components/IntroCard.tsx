b15c9eaad4cfe43cd8dbe2894b64176e4aaae3cd
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-12 18:43:21 -0700

    feat: add section for profile picture

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -3,14 +3,17 @@ import SmallNavbar from "./SmallNavbar";
 import AboutBlurb from "./widgets/AboutBlurb";
 import IntroCardTypewriterHeader from "./widgets/IntroCardTypewriterHeader";
 import Nameplate from "./widgets/Nameplate";
+import ProfilePicContainer from "./widgets/ProfilePicContainer";
 
 const IntroCard = () => {
   return (
     <div className="flex h-full w-full flex-col lg:h-[90%] lg:w-4/5 lg:flex-row-reverse lg:justify-between lg:drop-shadow-custom-large">
-      <div className="h-2/5 bg-[#0F1922] text-white lg:h-full lg:w-1/2 lg:rounded-r-md">
-        This should be an image
+      <div className="flex h-2/5 flex-col bg-gradient-to-br from-darksteel to-rich-black lg:h-full lg:w-1/2 lg:rounded-r-md">
+        <div className="flex h-full items-start justify-center lg:h-2/3 mt-6 lg:mt-16">
+          <ProfilePicContainer />
+        </div>
       </div>
-      <div className="flex flex-col h-3/5 items-start bg-white px-8 lg:h-full lg:w-1/2 lg:justify-start lg:rounded-l-md lg:px-16">
+      <div className="flex h-3/5 flex-col items-start bg-white px-8 lg:h-full lg:w-1/2 lg:justify-start lg:rounded-l-md lg:px-16">
         {/* Only visible on medium+ screens */}
         <SmallNavbar />
         <Nameplate />
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,10 +3,12 @@ import IntroCard from "./IntroCard";
 
 const LandingPage = () => {
   return (
-    <div className="flex h-screen w-screen flex-col justify-center bg-gradient-to-br from-capri to-cobalt lg:items-center">
+    <div className="flex h-screen w-screen flex-col justify-center bg-gradient-to-br from-azure to-cobalt lg:items-center">
       <IntroCard></IntroCard>
     </div>
   );
 };
 
 export default LandingPage;
+
+// bg-gradient-to-br from-capri to-cobalt
