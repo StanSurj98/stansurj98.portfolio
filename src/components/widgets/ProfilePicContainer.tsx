c7870cbd61065d99da0ce49648c88197e0856e3e
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-12 20:32:25 -0700

    style: sizing

--- src/components/widgets/ProfilePicContainer.tsx
+++ src/components/widgets/ProfilePicContainer.tsx
@@ -4,7 +4,7 @@ import ProfilePic from "../../assets/ProfilePic.jpg";
 
 const ProfilePicContainer = () => {
   return (
-    <div className="aspect-square h-2/3 overflow-hidden rounded-full bg-cover bg-center ring-4 ring-neutral-50 drop-shadow-2xl lg:h-3/4 lg:ring-8 2xl:h-2/3">
+    <div className="aspect-square h-4/5 overflow-hidden rounded-full bg-cover bg-center ring-4 ring-neutral-50 drop-shadow-2xl lg:h-3/4 lg:ring-8 2xl:h-full">
       <img src={ProfilePic} alt="picture of myself" />
     </div>
   );
