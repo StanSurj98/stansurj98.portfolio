5978040e4a500ca90aeb8ed38c1e9412c310b569
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-13 16:53:30 -0700

    test: init splide carousel

--- src/components/ProjectItems.tsx
+++ src/components/ProjectItems.tsx
@@ -1,21 +1,22 @@
 import bgImage from "../assets/interview_scheduler_homepage.png";
+import { SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 const ProjectItems = () => {
   return (
-    <a href="https://google.com" target="_blank">
-      <img
-        className="absolute inset-0 h-full w-full object-cover object-center"
-        src={bgImage}
-        alt={"Project Screenshot"}
-      />
-      {/* Gradient Overlay */}
-
-      <div className="absolute h-full w-full bg-gradient-to-t from-darksteel to-transparent">
-        {/* Details Box */}
-        <div className="absolute bottom-0 z-10">
-          <p>Date</p>
-          <h1>Title</h1>
-          <p className="text-l font-robotoCondensed">
+    <SplideSlide className="group h-screen overflow-hidden">
+      <a href="https://google.ca" target="_blank">
+        <img
+          className="inset-0 h-full w-full object-cover object-center transition-all duration-500 group-hover:scale-105"
+          src={bgImage}
+          alt={"Project Screenshot"}
+        />
+        <div className="absolute inset-0 right-0 h-full w-full bg-gradient-to-t from-darksteel to-transparent"></div>
+        <div className="max-w-4/5 absolute bottom-0 flex w-full flex-col items-center justify-center py-[25vh] px-14 lg:max-w-none lg:items-start lg:px-36">
+          <h1 className="fontSize-smallHeader tracking-wide mb-8 text-neutral-100">
+            Interview Scheduler
+          </h1>
+          <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
@@ -27,8 +28,8 @@ const ProjectItems = () => {
             PageMaker including versions of Lorem Ipsum
           </p>
         </div>
-      </div>
-    </a>
+      </a>
+    </SplideSlide>
   );
 };
 
