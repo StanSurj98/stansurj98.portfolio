942bdd28288fa02cbb0662c78ce83df14daf2f5d
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-12 17:10:36 -0700

    testing: mobile navbar

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -10,7 +10,8 @@ const IntroCard = () => {
       <div className="h-2/5 bg-[#0F1922] text-white lg:h-full lg:w-1/2 lg:rounded-r-md">
         This should be an image
       </div>
-      <div className="flex flex-col items-start bg-white px-8 lg:h-full lg:w-1/2 lg:justify-start lg:rounded-l-md lg:px-16">
+      <div className="flex flex-col h-3/5 items-start bg-white px-8 lg:h-full lg:w-1/2 lg:justify-start lg:rounded-l-md lg:px-16">
+        {/* Only visible on medium+ screens */}
         <SmallNavbar />
         <Nameplate />
         <h1 className="my-2 font-robotoCondensed text-xl font-bold text-azure lg:mb-4 lg:text-2xl 2xl:mb-6">
