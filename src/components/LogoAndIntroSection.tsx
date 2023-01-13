90e2696daa30aa7e13394c281f4b7edd34257ee2
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-12 19:48:50 -0700

    prettier

--- src/components/LogoAndIntroSection.tsx
+++ src/components/LogoAndIntroSection.tsx
@@ -7,25 +7,25 @@ import Nameplate from "./widgets/Nameplate";
 const LogoAndIntroSection = () => {
   return (
     <div className="min-h-3/5 relative flex h-full flex-col items-start bg-white px-8 pb-5 lg:h-full lg:w-1/2 lg:justify-start lg:rounded-l-md lg:px-16">
-        {/* Only visible on medium+ screens */}
-        <SmallNavbar />
-        <Nameplate />
-        <h1 className="my-2 font-robotoCondensed text-xl font-bold text-azure lg:mb-4 lg:text-2xl 2xl:mb-6">
-          ABOUT
-        </h1>
-        <IntroCardTypewriterHeader />
-        <AboutBlurb />
-        <div className="flex w-full items-center justify-center 2xl:mt-20">
-          <ButtonMain
-            onClick={() => {
-              console.log("clicked");
-            }}
-          >
-            View My Work
-          </ButtonMain>
-        </div>
+      {/* Only visible on medium+ screens */}
+      <SmallNavbar />
+      <Nameplate />
+      <h1 className="my-2 font-robotoCondensed text-xl font-bold text-azure lg:mb-4 lg:text-2xl 2xl:mb-6">
+        ABOUT
+      </h1>
+      <IntroCardTypewriterHeader />
+      <AboutBlurb />
+      <div className="flex w-full items-center justify-center 2xl:mt-20">
+        <ButtonMain
+          onClick={() => {
+            console.log("clicked");
+          }}
+        >
+          View My Work
+        </ButtonMain>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default LogoAndIntroSection
+export default LogoAndIntroSection;
