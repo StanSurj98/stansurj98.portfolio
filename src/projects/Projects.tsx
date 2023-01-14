fc6fbfe2ac8f4ee8be28d3d4e3da7ac400cbfb8c
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-13 19:32:46 -0700

    docs: store project info locally

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -3,25 +3,25 @@ export const projectsList = [
     name: "Gamer Gear",
     description: "Full-stack informercial site for gaming peripherals, visitors are able to see which gaming peripherals are currently dominating the market by game and used by the most professional players in the E-Sports scene. Built on NextJS with ReactTS.",
     url: "https://gamer-gear.vercel.app/home",
-    imageURL: "../assets/gamer_gear_collage.png"
+    imageURL: "gamer_gear_collage.png"
   },
   {
     name: "What's Brewin'",
     description: "Full-stack application on React, Express and PSQL helping beer lovers travel anywhere and view on a map, all the local breweries/brew-pubs in the area.",
     url: "https://github.com/StanSurj98/whats.brewin",
-    imageURL: "../assets/whats_brewin_collage.png"
+    imageURL: "whats_brewin_collage.png"
   },
   {
     name: "2D Point n' Shoot Game",
     description: 'A simple 2D "Point and Shoot" game built with vanilla JS, HTML and CSS as an exercise in collision detection, delta time, and array data structures.',
     url: "https://github.com/StanSurj98/Point_n_Shoot_2D_JS_Game",
-    imageURL: "../assets/2d_point_n_shoot.png"
+    imageURL: "2d_point_n_shoot.png"
   },
   {
     name: "Interview Scheduler",
     description: "A full stack application that allow users to create, edit and delete interview appointments.",
     url:"https://github.com/StanSurj98/interview-scheduler",
-    imageURL:"../assets/interview_scheduler_homepage.png"
+    imageURL:"interview_scheduler_collage.png"
   },
   // {
   //   name: "MindFULL+",
@@ -33,12 +33,12 @@ export const projectsList = [
     name: "Jungle",
     description: "A full stack e-commerce project with Ruby on Rails.",
     url: "https://github.com/StanSurj98/Jungle",
-    imageURL: "../assets/jungle_collage.png"
+    imageURL: "jungle_collage.png"
   },
   {
     name: "Tweeter",
     description: "A Twitter clone to learn front-end web development. Our first Single Page Application exercise during bootcamp.",
     url: "https://github.com/StanSurj98/Tweeter",
-    imageURL: "../assets/tweeter_collage.png"
+    imageURL: "tweeter_collage.png"
   },
 ]
