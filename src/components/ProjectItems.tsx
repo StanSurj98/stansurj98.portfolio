09b7905232bfcf988f8c68cbec7b6ce48d6b21e3
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-15 15:07:25 -0700

    feat: added effects to images

--- src/components/ProjectItems.tsx
+++ src/components/ProjectItems.tsx
@@ -13,10 +13,10 @@ type Props = {
 const ProjectItems = ({ name, description, imageURL, url }: Props) => {
   const cld = new Cloudinary({
     cloud: {
-      cloudName: 'dho5v3zlt'
-    }
-  })
-  const projectImage = cld.image(`portfolio-site/${imageURL}`)
+      cloudName: "dho5v3zlt",
+    },
+  });
+  const projectImage = cld.image(`portfolio-site/${imageURL}`);
   return (
     <SplideSlide className="group h-screen overflow-hidden">
       <a href={url} target="_blank">
@@ -25,7 +25,9 @@ const ProjectItems = ({ name, description, imageURL, url }: Props) => {
           src={}
           alt={`image of ${name} project`}
         /> */}
-        <AdvancedImage cldImg={projectImage} />
+        <div className="h-full w-full object-cover object-center transition-all duration-500 group-hover:scale-105">
+          <AdvancedImage height='100%' width='100%' style={{objectFit: 'cover', objectPosition: 'center'}} cldImg={projectImage} />
+        </div>
         <div className="absolute inset-0 right-0 h-full w-full bg-gradient-to-t from-darksteel to-transparent"></div>
         <article className="lg:prose-headings:lg max-w-4/5 prose absolute bottom-0 flex w-full flex-col items-center justify-center py-[15vh] px-14 prose-headings:underline-offset-8 lg:max-w-none lg:items-start lg:px-36 lg:prose-headings:underline">
           <h1 className="mb-8 font-robotoCondensed tracking-wide text-neutral-100">
