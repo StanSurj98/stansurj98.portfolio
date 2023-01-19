b8dd152f4cb0b54d9b7fdc983c8eec99bcc61fb1
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 20:10:45 -0700

    feat: add work section header and contact page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@
 import LandingPage from "./components/LandingPage";
 import WorkSection from "./components/WorkSection";
 import ContactSection from "./components/ContactSection";
+import SectionHeader from "./components/widgets/SectionHeader";
 
 function App() {
   return (
     <div className="overflow-x-hidden">
       {/* <Navbar></Navbar> */}
       <LandingPage />
+      <SectionHeader header={"WORK"} tagline={"Stay A While!"}/>
       <WorkSection />
-      {/* <ContactSection /> */}
+      <ContactSection />
     </div>
   );
 }
