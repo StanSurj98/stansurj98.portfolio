73ad39c80d3300c0406c80f3ed7e1420439cf61f
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 20:10:14 -0700

    style: changes for mobile layout

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,7 +3,10 @@ import IntroCard from "./IntroCard";
 
 const LandingPage = () => {
   return (
-    <section id="landingPage" className="relative flex h-screen w-screen flex-col justify-center bg-gradient-to-br from-azure to-cobalt lg:items-center border-2 border-blue-500">
+    <section
+      id="landingPage"
+      className="relative flex h-max w-screen flex-col justify-center lg:bg-gradient-to-br from-azure to-cobalt lg:h-screen lg:items-center"
+    >
       <IntroCard></IntroCard>
     </section>
   );
