6b93ff2860791ec9e2084c364c56dab92f92f046
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 20:05:45 -0700

    style: contact blurb section added

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,12 +1,12 @@
-import ContactBlurb from './widgets/ContactBlurb'
+import ContactBlurb from "./widgets/ContactBlurb";
 
 const ContactSection = () => {
   return (
-    <section className="flex h-screen w-screen items-center justify-between bg-gradient-to-br from-darksteel to-rich-black">
-        <ContactBlurb></ContactBlurb>
-        {/* <ContactForm></ContactForm> */}
+    <section className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-br from-darksteel to-rich-black px-10 py-8 lg:flex lg:justify-between">
+      <ContactBlurb></ContactBlurb>
+      {/* <ContactForm></ContactForm> */}
     </section>
   );
-}
+};
 
-export default ContactSection
+export default ContactSection;
--- src/components/widgets/ContactBlurb.tsx
+++ src/components/widgets/ContactBlurb.tsx
@@ -4,40 +4,41 @@ import { SiGmail } from "react-icons/si";
 
 const ContactBlurb = () => {
   return (
-    <div className="flex h-full w-1/2 flex-col items-center justify-center p-28 border-2 border-red-200">
-      <h1 className="w-full font-robotoCondensed text-2xl font-normal text-capri">
+    <div className="flex h-full w-full flex-col items-center justify-center lg:w-1/2">
+      <h1 className="fontSize-smallHeader mb-4 w-full font-robotoCondensed uppercase text-azure">
         CONTACT
       </h1>
-      <h1 className="my-7 w-full font-robotoCondensed text-5xl font-bold text-white">
-        Get In Touch
+      <h1 className="fontSize-smallHeader mb-12 w-full font-robotoCondensed text-white">
+        Get In Touch!
       </h1>
-      <p className="mb-3 w-full font-robotoCondensed text-xl font-light text-white">
+      <p className="text-l mb-12 w-full text-left font-robotoCondensed font-light text-white lg:text-xl 2xl:text-2xl">
         Have ideas for your dream application?
         <br />
         Do you want to collaborate on a project together?
         <br />
         <br />
-        Is there an opening at your super awesome workplace?
+        Is there an opportunity at your super awesome workplace?
         <br />
         I am currently seeking permanent role opportunities!
         <br />
         <br />
         So please, say hello!
       </p>
-      {/* on hover, make gmail icon to original color and email string to blue or something */}
-      <div className="flex w-full flex-row items-end justify-start font-robotoCondensed text-xl font-normal text-white grayscale saturate-0 hover:text-capri hover:grayscale-0 hover:saturate-100 ">
+      <div className="group flex w-full flex-row items-center justify-start text-white">
         <IconContext.Provider
           value={{
             className:
-              "ml-2 text-2xl cursor-pointer duration-200 ease-out hover:scale-110 hover:ease-out hover:text-azure",
+              "text-2xl cursor-pointer duration-200 ease-out group-hover:scale-110 group-hover:ease-out group-hover:text-azure",
           }}
         >
-          <img alt="gmail-logo" src={SiGmail} className="h-7 w-7 " />
+          <SiGmail />
+          <p className="text-l ml-4 cursor-pointer font-robotoCondensed tracking-wide duration-200 ease-out group-hover:scale-105 group-hover:text-azure group-hover:ease-out lg:text-xl">
+            <a href="mailto:stan.surjanto@gmail.com">stan.surjanto@gmail.com</a>
+          </p>
         </IconContext.Provider>
-        <p className="ml-3">stan.surjanto@gmail.com</p>
       </div>
     </div>
   );
 };
 
-export default ContactBlurb
+export default ContactBlurb;
