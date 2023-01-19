f36578d8aeca9122d3f40eb4ed985f7f64fcd11b
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 19:35:01 -0700

    rename: SectionHeader and is reusable

---
+++ src/components/widgets/SectionHeader.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  header: string;
+  tagline: string;
+}
+
+const WorkHeader = ({header, tagline}: Props) => {
+  return (
+    <div className="flex h-40 w-screen flex-col items-center justify-center">
+      <h1 className="fontSize-smallHeader mb-4 font-robotoCondensed uppercase text-azure ">
+        {header}
+      </h1>
+      <h1 className="fontSize-smallHeader font-robotoCondensed text-darksteel">
+        {tagline}
+      </h1>
+    </div>
+  );
+};
+
+export default WorkHeader;
