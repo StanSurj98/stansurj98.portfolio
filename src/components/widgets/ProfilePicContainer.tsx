8cb170af4092c565daa83774db3efff5cb008bf5
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 18:46:24 -0700

    style: mobile sizing fixes

--- src/components/widgets/ProfilePicContainer.tsx
+++ src/components/widgets/ProfilePicContainer.tsx
@@ -4,8 +4,8 @@ import ProfilePic from "../../assets/ProfilePic.jpg";
 
 const ProfilePicContainer = () => {
   return (
-    <div className="flex flex-col items-center justify-center mb-8 lg:h-3/5">
-      <div className="aspect-square h-64 overflow-hidden rounded-full bg-cover bg-center ring-4 ring-neutral-100 drop-shadow-2xl lg:h-3/4 lg:ring-8 2xl:h-full">
+    <div className="mb-8 flex flex-col items-center justify-center lg:h-3/5">
+      <div className="aspect-square h-64 overflow-hidden rounded-full bg-cover bg-center ring-4 ring-neutral-100 drop-shadow-2xl lg:h-3/4 lg:ring-8">
         <img src={ProfilePic} alt="My Profile Picture" />
       </div>
     </div>
