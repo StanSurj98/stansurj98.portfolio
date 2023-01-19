66ae28623cc8d8fa0eb5afc06408aadd83a7d9f8
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 18:40:31 -0700

    style: mobile sizes

--- src/components/widgets/ProfilePicContainer.tsx
+++ src/components/widgets/ProfilePicContainer.tsx
@@ -4,8 +4,10 @@ import ProfilePic from "../../assets/ProfilePic.jpg";
 
 const ProfilePicContainer = () => {
   return (
-    <div className="aspect-square h-4/5 overflow-hidden rounded-full bg-cover bg-center ring-4 ring-neutral-50 drop-shadow-2xl lg:h-3/4 lg:ring-8 2xl:h-full">
-      <img src={ProfilePic} alt="picture of myself" />
+    <div className="flex flex-col items-center justify-center mb-8 border-2 border-green-200 lg:h-3/5">
+      <div className="aspect-square h-64 overflow-hidden rounded-full bg-cover bg-center ring-4 ring-neutral-100 drop-shadow-2xl lg:h-3/4 lg:ring-8 2xl:h-full">
+        <img src={ProfilePic} alt="My Profile Picture" />
+      </div>
     </div>
   );
 };
