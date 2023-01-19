dd0e8d6cf2bfdd7a0879a457edf585599106152a
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 19:32:24 -0700

    feat: build own header component

---
+++ src/components/widgets/WorkHeader.tsx
@@ -0,0 +1,14 @@
+const WorkHeader = () => {
+  return (
+    <div className="flex h-40 w-screen flex-col items-center justify-center">
+      <h1 className="fontSize-smallHeader mb-4 font-robotoCondensed uppercase text-azure ">
+        Work
+      </h1>
+      <h1 className="fontSize-smallHeader font-robotoCondensed text-darksteel">
+        Hang Around a While!
+      </h1>
+    </div>
+  );
+};
+
+export default WorkHeader;
