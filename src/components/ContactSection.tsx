53f795b1775f98fdb8eca700705f45e3fe9e7fa5
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 20:16:33 -0700

    delete contact form for now

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -4,7 +4,6 @@ const ContactSection = () => {
   return (
     <section id="contact" className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-br from-darksteel to-rich-black px-10 py-8 lg:flex lg:justify-between">
       <ContactBlurb></ContactBlurb>
-      {/* <ContactForm></ContactForm> */}
     </section>
   );
 };
