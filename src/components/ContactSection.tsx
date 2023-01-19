d7bede39d68a09a2d0bd73f9e37d90714b87e655
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 20:10:30 -0700

    add section id for scrolling later

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -2,7 +2,7 @@ import ContactBlurb from "./widgets/ContactBlurb";
 
 const ContactSection = () => {
   return (
-    <section className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-br from-darksteel to-rich-black px-10 py-8 lg:flex lg:justify-between">
+    <section id="contact" className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-br from-darksteel to-rich-black px-10 py-8 lg:flex lg:justify-between">
       <ContactBlurb></ContactBlurb>
       {/* <ContactForm></ContactForm> */}
     </section>
