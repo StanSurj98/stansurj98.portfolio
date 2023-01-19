8902410c3cb1efe302b2e7b9abe3aa2ecd1d6d50
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 20:09:31 -0700

    style: pull out header to own component

--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -18,21 +18,10 @@ const WorkSection = () => {
     }
   );
   return (
-    // this mt-72 is a hacky fix for mobile, not sure why it's overlapping the landing page intro card...
-    <section id="work" className="w-full h-screen pt-16 lg:mt-0 border-2 border-green-200">
-      {/* <div className="mx-auto w-10/12 text-center 2xl:w-[80%]">
-        <h2 className="fontSize-smallHeader mb-4 font-robotoCondensed uppercase text-azure ">
-          Work
-        </h2>
-        <h3 className="fontSize-smallHeader mb-8 font-robotoCondensed text-darksteel">
-          Hang Around a While!
-        </h3>
-      </div>
-      <div>
-        <Splide hasTrack={false} className="max-w-full">
+    <section id="work" className="w-screen h-screen">
+        <Splide hasTrack={false} className="max-w-full overflow-hidden">
           <SplideTrack>{projectItems}</SplideTrack>
         </Splide>
-      </div> */}
     </section>
   );
 };
