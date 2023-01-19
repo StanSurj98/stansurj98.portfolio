18cf51d68e10b184e6906567c862af0cdbdf0904
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-18 20:10:01 -0700

    fix: splide layout spacing for mobile

--- src/components/ProjectItems.tsx
+++ src/components/ProjectItems.tsx
@@ -11,25 +11,27 @@ type Props = {
 };
 
 const ProjectItems = ({ name, description, imageURL, url }: Props) => {
+  // Cloudinary Setup for Project Images from CMS
   const cld = new Cloudinary({
     cloud: {
       cloudName: "dho5v3zlt",
     },
   });
   const projectImage = cld.image(`portfolio-site/${imageURL}`);
+
   return (
-    <SplideSlide className="group h-screen overflow-hidden">
+    <SplideSlide className="group relative h-screen w-screen overflow-hidden">
+      {/* The entire slide is an <a> with href to project */}
       <a href={url} target="_blank">
-        {/* <img
-          className="inset-0 h-full w-full object-cover object-center transition-all duration-500 group-hover:scale-105"
-          src={}
-          alt={`image of ${name} project`}
-        /> */}
+        {/* Layer at most-back is image with hover effect */}
         <div className="h-full w-full object-cover object-center transition-all duration-500 group-hover:scale-105">
-          <AdvancedImage height='100%' width='100%' style={{objectFit: 'cover', objectPosition: 'center'}} cldImg={projectImage} />
+          <AdvancedImage cldImg={projectImage} />
         </div>
-        <div className="absolute inset-0 right-0 h-full w-full bg-gradient-to-t from-darksteel to-transparent"></div>
-        <article className="lg:prose-headings:lg max-w-4/5 prose absolute bottom-0 flex w-full flex-col items-center justify-center py-[15vh] px-14 prose-headings:underline-offset-8 lg:max-w-none lg:items-start lg:px-36 lg:prose-headings:underline">
+        {/* Next layer is semi-transparent overlay, absolute but relative to this slide parent */}
+        <div className="absolute inset-0 h-full w-full bg-gradient-to-t from-darksteel to-transparent"></div>
+
+        {/* Top most layer is project descriptions and such */}
+        <article className="max-w-4/5 lg:prose-headings:lg prose absolute bottom-0 flex flex-col items-center justify-center px-10 pb-[15vh]  prose-headings:underline-offset-8 lg:max-w-none lg:items-start lg:px-36 lg:prose-headings:underline">
           <h1 className="mb-8 font-robotoCondensed tracking-wide text-neutral-100">
             {name}
           </h1>
