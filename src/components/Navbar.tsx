a39f922e245e1cf1ce1b40cf1598846106f0b98d
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-21 14:21:00 -0700

    feat: build desktop and mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,29 +22,43 @@ const Navbar = () => {
   });
 
   return (
-    <nav className="fixed top-0 z-50 flex h-16 w-full items-center justify-between bg-white">
-      <div className="mx-auto flex w-[90%] items-center justify-between">
-        <Logo />
+    <>
+      {/* opaque underlayer to navbar */}
+      <div className="bg-gradient-to-br from-darksteel to-rich-black opacity-90 fixed top-0 z-40 h-16 w-full"></div>
 
-        <button className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
-          <IconContext.Provider
-            value={{
-              className:
-                "text-2xl cursor-pointer duration-200 ease-out hover:scale-110 hover:ease-out hover:text-azure",
-            }}
-          >
-            {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
-          </IconContext.Provider>
-        </button>
-      </div>
-
-      {isOpen && (
-        <div className="fixed top-0 z-50 mt-16 flex h-max w-full flex-col items-center justify-center gap-8 bg-white py-8">
-          {navChildren}
-          <SocialsIcons />
+      <nav className="fixed top-0 z-50 flex h-16 w-full items-center justify-between bg-white lg:bg-transparent">
+        {/* desktop navbar visible on large screens only */}
+        <div className="mx-auto hidden w-[90%] items-center justify-between lg:flex">
+          <Logo alt={true} />
+          <div className="hidden items-center justify-center gap-8 text-white lg:flex">
+            {navChildren}
+            <SocialsIcons />
+          </div>
         </div>
-      )}
-    </nav>
+
+        {/* mobile navbar visible on small screens only */}
+        <div className="mx-auto flex w-[90%] items-center justify-between lg:hidden">
+          <Logo />
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <IconContext.Provider
+              value={{
+                className:
+                  "text-2xl cursor-pointer duration-200 ease-out hover:scale-110 hover:ease-out hover:text-azure",
+              }}
+            >
+              {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+            </IconContext.Provider>
+          </button>
+        </div>
+
+        {isOpen && (
+          <div className="fixed top-0 z-50 mt-16 flex h-max w-full flex-col items-center justify-center gap-8 bg-white py-8 drop-shadow-xl">
+            {navChildren}
+            <SocialsIcons />
+          </div>
+        )}
+      </nav>
+    </>
   );
 };
 
