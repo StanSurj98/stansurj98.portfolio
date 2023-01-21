8c957c07742f89213c4f3f68c56729e441340ef1
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-21 14:19:35 -0700

    style: add hover effects

--- src/components/widgets/Logo.tsx
+++ src/components/widgets/Logo.tsx
@@ -1,13 +1,17 @@
-import React from "react";
-import logoImage from "../../assets/ss_logo_nobg2.png";
+import logoMain from "../../assets/ss_logo_main_nobg.png";
+import logoAlt from "../../assets/ss_logo_alt_nobg.png";
 
-const Logo = () => {
+type Props = {
+  alt?: boolean;
+};
+
+const Logo = ({ alt }: Props) => {
   return (
     <a href="#landingPage" className="flex items-center">
       <img
-        src={logoImage}
+        src={alt ? logoAlt : logoMain}
         alt="Personal Logo for StanSurj98"
-        className="cursor-pointer w-14 lg:w-16 2xl:w-20"
+        className="w-14 cursor-pointer duration-200 ease-out hover:scale-105 hover:ease-out lg:w-16 2xl:w-20"
       />
     </a>
   );
