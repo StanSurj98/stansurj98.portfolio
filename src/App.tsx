7517283809aa65ba5638bb870c02fc1e014d418d
Stanley Surjanto <stan.surjanto@gmail.com>
2023-01-24 11:53:14 -0700

    feat: enable desktop navbar on all pages for now

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import LandingPage from "./components/LandingPage";
 import WorkSection from "./components/WorkSection";
 import ContactSection from "./components/ContactSection";
 import SectionHeader from "./components/widgets/SectionHeader";
+import DesktopNavbar from "./components/DesktopNavbar";
 
 function App() {
   return (
     <div className="overflow-x-hidden">
       <MobileNavbar></MobileNavbar>
+      <DesktopNavbar></DesktopNavbar>
       <LandingPage />
       <SectionHeader header={"WORK"} tagline={"Stay A While!"}/>
       <WorkSection />
